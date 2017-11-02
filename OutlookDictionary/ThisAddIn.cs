b9ce1e45256462a293aa2fd00efe83961a181535
Yadunandan Pillai <ytpillai@gmail.com>
2017-11-02 13:18:28 -0400

    Fixed program to edit HTML with a popup on Abbreviations. Also added data file writing and reading functionality (CSV, Excel later)

--- OutlookDictionary/ThisAddIn.cs
+++ OutlookDictionary/ThisAddIn.cs
@@ -5,33 +5,67 @@ using System.Text;
 using System.Xml.Linq;
 using Outlook = Microsoft.Office.Interop.Outlook;
 using Office = Microsoft.Office.Core;
+using System.Windows.Forms;
+using System.IO;
 
 namespace OutlookDictionary
 {
+
+
     public partial class ThisAddIn
     {
-        private void ThisAddIn_Startup(object sender, System.EventArgs e)
-        {
-        }
+        Outlook.Inspectors inspectors;
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
             // Note: Outlook no longer raises this event. If you have code that 
             //    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
         }
-
-        #region VSTO generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InternalStartup()
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
-            this.Startup += new System.EventHandler(ThisAddIn_Startup);
-            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+            inspectors = this.Application.Inspectors;
+            inspectors.NewInspector +=
+            new Microsoft.Office.Interop.Outlook.InspectorsEvents_NewInspectorEventHandler(Inspectors_NewInspector);
+
+
         }
+        void Inspectors_NewInspector(Microsoft.Office.Interop.Outlook.Inspector Inspector)
+        {
+            Outlook.MailItem mailItem = Inspector.CurrentItem as Outlook.MailItem;
+            if (mailItem != null)
+            {
+                if (mailItem.EntryID != null)
+                {
+                    mailItem.HTMLBody = mailItem.HTMLBody.Replace("ggfsdt", "<a href='Go Go Forward So Dis Toward'>ggfsdt</a>");
+                    MessageBox.Show("asd");
+                }
+
+            }
+
+
+
+        }
+        /*private string GetDataFile()
+        {
+            string userSettingsPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            // build company folder full path
+            string companyFolder = Path.Combine(userSettingsPath, "OutlookDictionary");
+
+            if (!Directory.Exists(companyFolder))
+            {
+                Directory.CreateDirectory(companyFolder);
+
+                string fullSettingsPath = Path.Combine(companyFolder, "abbrev-list.csv");
+
+                return fullSettingsPath;
+            }
+
+
+            userSettingsPath = Path.Combine(companyFolder, "abbrev-list.csv");
+            return userSettingsPath;
+        }*/
+
         
-        #endregion
     }
 }
